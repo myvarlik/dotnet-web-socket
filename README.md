b721f3a4d679b27996a49a83be674c77ca446ab8
Muhammed Yahya VARLIK <myvarlik@icloud.com>
2023-08-22 13:27:21 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotnet-web-socket
+
+
+In its simplest form, .net websocket server and client.
---
+++ server/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+            httpListener.Start();
+            Console.WriteLine("WebSocket sunucusu başlatıldı. Bağlantı bekleniyor...");
+
+            while (true)
+            {
+                HttpListenerContext context = await httpListener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    ProcessWebSocketRequest(context);
+                }
+            }
+        }
+
+        static async void ProcessWebSocketRequest(HttpListenerContext context)
+        {
+            HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+            WebSocket socket = webSocketContext.WebSocket;
+            Console.WriteLine("Yeni bağlantı oluşturuldu.");
+
+            byte[] buffer = new byte[1024];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine("Gelen Mesaj: " + receivedMessage);
+
+                    await socket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+
+            Console.WriteLine("Bağlantı kapatıldı.");
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Connection closed", CancellationToken.None);
+        }
+    }
+}
